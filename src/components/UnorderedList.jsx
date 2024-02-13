5f718305a054343f3627462586b4dea49ddbc8f1
Yarialis Guzman <yarialis.guzman@gmail.com>
2024-02-13 00:22:19 -0500

    style the unordered list of posts

--- src/components/UnorderedList.jsx
+++ src/components/UnorderedList.jsx
@@ -1,7 +1,8 @@
 import Post from './Post';
+import classes from './UnorderedList.module.css'
 function UnorderedList(){
  return(
-    <ul>
+    <ul className={classes.posts}>
     <Post author="Jasmine" body="what a loser"></Post>
     <Post author="Rory" body="Did you find the body?"/>
     </ul>
