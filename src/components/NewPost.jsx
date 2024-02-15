d1111b475cb3844708f48c5673db3e60bb30b22a
Yarialis Guzman <yarialis.guzman@gmail.com>
2024-02-15 17:29:18 -0500

    add event listener to the post modal

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,12 +1,18 @@
 import classes from './NewPost.module.css';
+import { useState } from 'react';
 
 function NewPost() {
+    const [currentBody, setCurrentBody]=useState('');
+    function bodyChangeHandler(e){
+       setCurrentBody(e.target.value);
+    }   
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={bodyChangeHandler}/>
       </p>
+      <p>{currentBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
