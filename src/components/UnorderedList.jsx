9295bdf0d05ab3f51477a2c3251c5dc9f3481eb6
Yarialis Guzman <yarialis.guzman@gmail.com>
2024-02-15 21:55:28 -0500

    take away dummy post and create submitted post data object

--- src/components/UnorderedList.jsx
+++ src/components/UnorderedList.jsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import Post from './Post';
 import classes from './UnorderedList.module.css'
 import NewPost from './NewPost';
@@ -7,25 +5,11 @@ import Modal from './Modal';
 
 
 function UnorderedList({willPost, onStopPosting}){
-   const [currentBody, setCurrentBody]=useState('');
-   const [currentAuthor, setCurrentAuthor]=useState('');
-  
-
-
-   function bodyChangeHandler(e){
-      setCurrentBody(e.target.value);
-   }
-
-   function authorChangeHandler(e){
-      setCurrentAuthor(e.target.value);
-   }
-
- 
 
    let modalContent;
    if(willPost){
       modalContent= <Modal onClose={onStopPosting}>
-      <NewPost onBodyUpdates={bodyChangeHandler} onAuthorUpdates={authorChangeHandler}></NewPost>
+      <NewPost onCancel={onStopPosting}></NewPost>
       </Modal>
    }
 
@@ -33,7 +17,6 @@ function UnorderedList({willPost, onStopPosting}){
    <>
    {modalContent}
     <ul className={classes.posts}>
-    <Post author={currentAuthor} body={currentBody}></Post>
     <Post author="Rory" body="Did you find the body?"/>
     </ul>
     </>
