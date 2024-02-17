277f7984bb9a6f8c451489656ab802518a9b16df
Yarialis Guzman <yarialis.guzman@gmail.com>
2024-02-16 21:34:44 -0500

    add react-dom-router hook and create pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import NewPost from './components/NewPost.jsx'
+import RootLayout from './routes/RootLayout.jsx'
 
+const router= createBrowserRouter([
+  {path:'/', element:<RootLayout/>, children:[
+    {path:'/', element:<App/>},
+    {path:'/creat-post', element:<NewPost/>}
+  ]}
+
+
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
